4625d08ed61720ee81212cc1fef85bc1eac5647b
abdo1234510 <92454005+Abdelrahman-Wael-1029@users.noreply.github.com>
2025-03-08 23:43:24 +0200

    init

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,18 @@
+/*
+#3d95e8
+#004eb6
+#e6f4f1
+#683e00
+
+*/
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff3d95e8);
+  static const Color secondaryColor = Color(0xff004eb6);
+  static const Color backgroundColor = Color(0xffe6f4f1);
+  static const Color buttonColor = Color(0xff683e00);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+}
---
+++ lib/utils/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+var lightTheme = ThemeData(
+  brightness: Brightness.light,
+
+  textTheme: TextTheme(
+    bodyLarge: TextStyle(
+      
+    )
+  )
+);
