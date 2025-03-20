5d085b2520b126cdd7b21b0784c9e866931ab45e
abdelrahman <abdo12345678mo@gmail.com>
2025-03-20 17:40:52 +0200

    connect firebase files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lip_reading/bloc_observer.dart';
 import 'package:lip_reading/cubit/lip_reading/lip_reading_cubit.dart';
+import 'package:lip_reading/firebase_options.dart';
 import 'package:lip_reading/utils/app_route.dart';
 import 'package:lip_reading/utils/theme.dart';
 import 'package:lip_reading/screens/splash_screen/splash_screen.dart';
@@ -10,7 +12,10 @@ import 'package:responsive_framework/responsive_wrapper.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
-
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(create: (context) => LipReadingCubit()),
